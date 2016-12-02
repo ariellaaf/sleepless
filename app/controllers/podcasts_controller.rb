c735ee6017f17cfd894bf2485bb985c4bed5a395
ariellaaf <a88854@aup/edu>
2016-12-02 17:14:54 +0000

    routed and created podcasts controller with new method

---
+++ app/controllers/podcasts_controller.rb
@@ -0,0 +1,11 @@
+class PodcastsController < ApplicationController
+ def new
+ end
+end
+
+#A controller is simply a class that is defined 
+#to inherit from ApplicationController. 
+#It's inside this class that you'll define methods 
+#that will become the actions for this controller. 
+#These actions will perform CRUD operations 
+#on the articles within our system.
